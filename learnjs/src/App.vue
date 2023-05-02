167a64f7df13ca875383bf4f1ac73dcb31d49e11
mohammad reza <mamalreza83@gmil.com>
2023-05-02 23:37:27 +0330

    I add a baner for bazargam and panel for tomato

--- learnjs/src/App.vue
+++ learnjs/src/App.vue
@@ -1,60 +1,31 @@
 <script setup lang="ts">
-  const loger = () =>{
-    console.log("done")
-  }
+  
 </script>
 
 <template>
-  <div class="btn">
-
-    <button >this is logo</button>
+  <img class="banner" src="./banner.png" alt="cant load">
+  <div class="tomato">
+    <img src="./tomato.jpg" alt="cant load">
+    <h1>tomato</h1>
+    <h2>30.000 T</h2>
   </div>
-  <div class="search">
-    <input type="text" id="value">
-  </div>
-  <div class="btnsearch">
-    <button @click="loger">search</button>
-  </div>
-  
-    <button class="pruduct">first prudus</button>
-
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-.btn{
+.banner{
   position: absolute;
   top: 5%;
-  right: 2%;
+  left: 25%;
 }
 
-.search{
+.tomato{
   position: absolute;
-  top: 6%;
-  right: 20%;
+  top: 50%;
+  left: 5%;
+  padding: 10px;
+  border: 5px solid rgb(255, 147, 6);
+  border-radius: 10px;
+  
 }
-.btnsearch{
-  position: absolute;
-  top: 5%;
-  right: 45%;
-}
-.pruduct{
-  right: 2%;
-  top: 15%;
-  width: 500px;
-  background-color: red;
-}
-
 
 </style>
