b5b5911b9a839681ec2379c73feb85a7f609b11a
Mohammadreza1383b <mamalreza83@gmai.com>
2023-05-11 20:40:24 -0700

    symple project in course that use data and even click

---
+++ app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            title : "cell phone",
+            price : 45,
+            country : 'iran'
+        }
+    }
+})
+
+app.mount('#app')
