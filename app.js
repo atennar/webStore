1a49535e5569379ca12ebf2bdb79afc200ac9633
Mohammadreza1383b <mamalreza83@gmai.com>
2023-05-14 09:43:15 -0700

    I learn that how work by arrays of object and read them

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const app = Vue.createApp({
             title : "cell phone",
             price : 45,
             country : 'iran',
-            books:['ali','mohammad','reza','amir']
+            names:[
+                {name:'ali',family:'baghery',age:13},
+                {name:'reza',family:'zare',age:13},
+                {name:'mohammad',family:'fallah',age:13},
+                {name:'amir',family:'nejaty',age:13}
+            ]
         }
     },
 //this is method property that we can difine a function
