46175dd1d47b332199135367f0fac4fd1704ac73
Mohammadreza1383b <mamalreza83@gmai.com>
2023-05-13 19:39:48 -0700

    i learn use <v-if> for show an div and link with methods

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const app = Vue.createApp({
     data(){
         return{
+            show :true,
             title : "cell phone",
             price : 45,
-            country : 'iran'
+            country : 'iran',
         }
     },
 //this is method property that we can difine a function
     methods : {
         Titlechange(name){
             this.title = name
+        },
+
+        showmethod (){
+            this.show = !this.show
         }
     }
 })
