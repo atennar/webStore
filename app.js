98a2b9cb84c287a47831af7ceff5e96fdba31915
Mohammadreza1383b <mamalreza83@gmai.com>
2023-05-17 09:06:17 -0700

    add to butem for change them and delete a box by filter

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const app = Vue.createApp({
             price : 45,
             country : 'iran',
             names:[
-                {name:'tomato',property:'it is good for your body',price:20000 ,image:'tomato.jpg' ,isfav:false , id:0},
-                {name:'benana',property:'it is good for your body',price:30000 ,image:'benana.jpg' ,isfav:false , id:1},
-                {name:'orange',property:'it is good for your body',price:40000 , image:'orange.jpg' ,isfav:false ,id:2},
-                {name:'watermelone',property:'it is good for your body',price:50000 , image:'watermelone.jpg' ,isfav:false , id:3 }
+                {name:'tomato',property:'it is good for your body',price:20000 ,image:'tomato.jpg' ,isfav:false , del:false},
+                {name:'benana',property:'it is good for your body',price:30000 ,image:'benana.jpg' ,isfav:false , del:false},
+                {name:'orange',property:'it is good for your body',price:40000 , image:'orange.jpg' ,isfav:false ,del:false},
+                {name:'watermelone',property:'it is good for your body',price:50000 , image:'watermelone.jpg' ,isfav:false , del:false }
             ]
         }
     },
@@ -19,14 +19,18 @@ const app = Vue.createApp({
     methods : {
         addfav(name) {
             name.isfav = !name.isfav
-        }
+        },
 
+        addtrush(name){
+            name.del = !name.del
+            return name
+        },
 
     },
 
     computed : {
         filterBooks(){
-            return this.names.filter(book => book.isfav === false)
+            return this.names.filter(name => name.del === false)
         }
     }
 })
