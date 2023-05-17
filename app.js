07a160f7cfa1ee776a4433f4d8b3d29456ad89bd
Mohammadreza1383b <mamalreza83@gmai.com>
2023-05-17 07:27:54 -0700

    I add update for last commit more info in comment

--- app.js
+++ app.js
@@ -8,16 +8,21 @@ const app = Vue.createApp({
             price : 45,
             country : 'iran',
             names:[
-                {name:'tomato',property:'it is good for your body',price:20000 ,image:'tomato.jpg' ,isfav:false},
-                {name:'benana',property:'it is good for your body',price:30000 ,image:'benana.jpg' ,isfav:true },
-                {name:'orange',property:'it is good for your body',price:40000 , image:'orange.jpg' ,isfav:true },
-                {name:'watermelone',property:'it is good for your body',price:50000 , image:'watermelone.jpg' ,isfav:false }
+                {name:'tomato',property:'it is good for your body',price:20000 ,image:'tomato.jpg' ,isfav:false , id:0},
+                {name:'benana',property:'it is good for your body',price:30000 ,image:'benana.jpg' ,isfav:true , id:1},
+                {name:'orange',property:'it is good for your body',price:40000 , image:'orange.jpg' ,isfav:true ,id:2},
+                {name:'watermelone',property:'it is good for your body',price:50000 , image:'watermelone.jpg' ,isfav:false , id:3 }
             ]
         }
     },
 //this is method property that we can difine a function
     methods : {
-       
+        addfav(name) {
+            name.isfav = !name.isfav
+            return{
+                name
+            }
+        }
 
 
     }
