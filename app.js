1460704801e6dcd4cb6e527ddd1101a5d1ce37c8
Mohammadreza1383b <mamalreza83@gmai.com>
2023-05-17 07:00:56 -0700

    I learn active class that for special box

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const app = Vue.createApp({
             price : 45,
             country : 'iran',
             names:[
-                {name:'tomato',property:'it is good for your body',price:20000 ,image:'tomato.jpg'},
-                {name:'benana',property:'it is good for your body',price:30000 ,image:'benana.jpg' },
-                {name:'orange',property:'it is good for your body',price:40000 , image:'orange.jpg' },
-                {name:'watermelone',property:'it is good for your body',price:50000 , image:'watermelone.jpg' }
+                {name:'tomato',property:'it is good for your body',price:20000 ,image:'tomato.jpg' ,isfav:false},
+                {name:'benana',property:'it is good for your body',price:30000 ,image:'benana.jpg' ,isfav:true },
+                {name:'orange',property:'it is good for your body',price:40000 , image:'orange.jpg' ,isfav:true },
+                {name:'watermelone',property:'it is good for your body',price:50000 , image:'watermelone.jpg' ,isfav:false }
             ]
         }
     },
