a53a7fa91b75e65e595de1401aca88e794890592
Mohammadreza1383b <mamalreza83@gmai.com>
2023-05-17 05:51:51 -0700

    I use dinamic image url for load fruit images

--- app.js
+++ app.js
@@ -1,42 +1,23 @@
 const app = Vue.createApp({
     data(){
         return{
+            link:"https://www.google.com",
             X:0,
             show :true,
             title : "cell phone",
             price : 45,
             country : 'iran',
             names:[
-                {name:'tomato',property:'it is good for your body',price:20000 , count:0 ,hasbuy:false},
-                {name:'benana',property:'it is good for your body',price:30000 ,count:0 ,hasbuy:false},
-                {name:'orange',property:'it is good for your body',price:40000 ,count:0 ,hasbuy:false},
-                {name:'watermelone',property:'it is good for your body',price:50000 ,count:0 ,hasbuy:false}
+                {name:'tomato',property:'it is good for your body',price:20000 ,image:'tomato.jpg'},
+                {name:'benana',property:'it is good for your body',price:30000 ,image:'benana.jpg' },
+                {name:'orange',property:'it is good for your body',price:40000 , image:'orange.jpg' },
+                {name:'watermelone',property:'it is good for your body',price:50000 , image:'watermelone.jpg' }
             ]
         }
     },
 //this is method property that we can difine a function
     methods : {
-        Titlechange(name){
-            this.title = name
-        },
-
-        showmethod (){
-            this.show = !this.show
-        },
-
-        mousehandle() {
-            console.log('this even done')
-        },
-
-        mouseevent(e){
-            this.X = e.offsetX
-        },
-
-        buy(fruit) {
-            fruit.count++
-            fruit.hasbuy = true
-            return fruit
-        }
+       
 
 
     }
